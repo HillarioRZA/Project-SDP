25afdaeab8f0f293d4e47d332f7e7ce6d3e25868
Muhammad robah <86682953+Daehoo0@users.noreply.github.com>
2023-12-05 19:08:38 +0700

    ndak ero

--- Project_SDP/src/pages/checkout.jsx
+++ Project_SDP/src/pages/checkout.jsx
@@ -1,5 +1,16 @@
 import React from "react";
 
 export const Checkout = () => {
-  return <div>out</div>;
+  return (
+    <>
+    <div>
+      <div>
+        <h2>Payment</h2>
+      </div>
+      <div>
+        
+      </div>
+    </div>
+    </>
+  )
 };
