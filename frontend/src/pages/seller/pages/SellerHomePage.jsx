3bad2dd37c769a42a5030a1c90535b8ebda3d41f
AlexanderRC01 <alexanderaldi01@gmail.com>
2023-12-20 22:13:26 +0700

    Improving statistics

--- frontend/src/pages/seller/pages/SellerHomePage.jsx
+++ frontend/src/pages/seller/pages/SellerHomePage.jsx
@@ -1,16 +1,25 @@
 import { Grid } from '@mui/material';
+import React, { useEffect, useState } from 'react';
 import SalesCard from '../components/SalesCard';
 import SalesChart from '../components/SalesChart';
 import { useDispatch, useSelector } from "react-redux";
+import { getProductsbySeller, getSpecificProducts} from '../../../redux/userHandle';
 
 const SellerHomePage = () => {
-  const {specificProductData, sellerProductData} = useSelector(state => state.user);
+  const {currentUser, specificProductData, sellerProductData} = useSelector(state => state.user);
+  
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getProductsbySeller(currentUser._id));
+    dispatch(getSpecificProducts(currentUser._id, "getOrderedProductsBySeller"));
+  }, [dispatch, currentUser._id])
 
   return (
     <Grid container spacing={3} sx={{ padding: "9px" }}>
 
       <Grid item xs={12} sm={6} md={3}>
-        <SalesCard title="Weekly Sales" total={specificProductData} color='primary' icon={'ant-design:carry-out-filled'} />
+        <SalesCard title="Weekly Sales" total={specificProductData.length} color='primary' icon={'ant-design:carry-out-filled'} />
       </Grid>
 
       <Grid item xs={12} sm={6} md={3}>
