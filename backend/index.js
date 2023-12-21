ce6c0e8b364b5aee064ad6d00b35b96bd304354b
RezaPratama <63398405+HillarioRZA@users.noreply.github.com>
2023-12-22 00:55:19 +0700

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -16,14 +16,10 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(cors({
-  origin: 'https://project-sdp-fe.vercel.app'
+  origin: ['https://project-sdp-fe.vercel.app'],
+  methods: ["POST","GET"],
+  credentials:true
 }));
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
 app.use(express.json({ limit: "10mb" }));
 
 
