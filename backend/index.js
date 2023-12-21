9b34bf6a5df2c75e89b4aea2fc0824d322ed7aab
RezaPratama <63398405+HillarioRZA@users.noreply.github.com>
2023-12-22 00:50:19 +0700

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(cors({
-  origin: 'https://project-sdp-fe.vercel.app/'
+  origin: 'https://project-sdp-fe.vercel.app'
 }));
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
