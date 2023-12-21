6c7c9242cdb60b1cf81d396fa2a19c3bd54845b9
RezaPratama <63398405+HillarioRZA@users.noreply.github.com>
2023-12-22 00:18:07 +0700

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -14,8 +14,18 @@ const Routes = require("./routes/route.js");
 
 const PORT = process.env.PORT || 5000;
 
-app.use(express.json({ limit: '10mb' }));
 app.use(cors());
+app.use(cors({
+  origin: 'https://project-sdp-fe.vercel.app/'
+}));
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+app.use(express.json({ limit: '10mb' }));
+
 
 // console.log('MONGO_URL:', MONGO_URL);
 // console.log('JWT_SECRET:', jwtSecret);
